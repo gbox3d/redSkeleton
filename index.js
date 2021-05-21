a46ad3d90ec74e4d5499075f2ad6acf23a36e290
gbox3d <gbox3d@gmail.com>
2021-05-21 17:30:58 +0900

    .....

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-import {CCoreApp} from './coreApp.js';
+import YAML from 'yaml'
+import fs from 'fs'
+import { CCoreApp } from './coreApp.js';
+
+let settings = {
+    port: 8080,
+    upload_path: './uploads',
+    web_content_path: './web_content',
+}
+if (!fs.existsSync('settings.yaml')) {
+    console.log('create settings.yaml')
+    fs.writeFileSync('settings.yaml',YAML.stringify(settings))
+}
+else {
+    settings = YAML.parse(fs.readFileSync('settings.yaml','utf-8'))
+}
+
 
 let app = new CCoreApp({
-    port:8080,
-    upload_path : "./uploads",
-    web_content_path : "./web_content"
+    port: settings.port,
+    upload_path: settings.upload_path,
+    web_content_path: settings.web_content_path
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
 # redSkeleton
+익스프레이스를 쓰지않고 가볍게 만든 api 개발 프레임워크 입니다.  
+외부 모듈은 yaml,node-static 만 사용하였습니다.  
+
+## 사용법
+반드시 클로닝 이후에 npm install을 한번 실행해주세요.  
+index.js 가 메인 모듈입니다. 실행은 아래예와 같이 합니다.  
+
+```sh
+npm install # 처음 한번만
+node index.js
+```
+
+처음에 setting.yaml 파일이 자동으로 생성됩니다.  
+* port 는 서비스 포트 번호입니다.  
+* upload_path 업로드 api 가 파일을 저장할 위치를 지정합니다.  
+* web_content_path html 파일과 같은 정적웹파일들이 있을 곳을 지정합니다.  
+
+
+
